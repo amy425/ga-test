1e6f70742f58306f88a40f3085a0d6f7d96276fe
Amy Steel <amylouisesteel@hotmail.co.uk>
2024-01-07 16:11:34 +0000

    Test fix

--- src/home.js
+++ src/home.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 export default function Home() {
+  const MEASUREMENT_ID = "G-D9E6DEPHJ9";
+  ReactGA.initialize(MEASUREMENT_ID, { debug: true });
   ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <div className="Home">
