2f4fac80d702ac89da1c626c0828004b22d00c84
Amy Steel <amylouisesteel@hotmail.co.uk>
2024-01-07 15:52:37 +0000

    Added ga4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Home from "./home";
 
 export default function App() {
+  const MEASUREMENT_ID = "G-D9E6DEPHJ9";
+  ReactGA.initialize(MEASUREMENT_ID, { debug: true });
+
+  // Send a custom event
+  ReactGA.event({
+    category: "your category",
+    action: "your action",
+    label: "your label", // optional
+    value: 99, // optional, must be a number
+    nonInteraction: true, // optional, true/false
+    transport: "xhr", // optional, beacon/xhr/image
+  });
+
   return (
     <div className="App">
       <Home />
